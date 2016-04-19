9094913049a6c48397563b8429118281d5863653
Josh Bryant <kaytal@gmail.com>
2016-04-18 22:07:37 -0500

    Added correct fonts changed the fonts in the styles finished the last bit of styles for the categories pages

--- category.php
+++ category.php
@@ -1,3 +1,4 @@
+<div id="top"></div>
 <h1 class="text-center"><?php single_cat_title(); ?></h1>
 
 <?php $cat_id=get_query_var('cat'); ?>
@@ -5,17 +6,21 @@
 <?php $catPost = get_posts('cat=<?php $cat_id ?>&posts_per_page=-1'); ?>
 <?php $i = 0 ?>
 <div class="tabbable">
-  <ul class="nav nav-stacked col-md-3">
-    <?php foreach ($catPost as $post) : setup_postdata($post); ?>
-      <li class="cat-tab-nav"><a href="<?php echo '#' . $post->post_name ?>"><?php the_title(); ?></a></li>
-      <?php $i++ ?>
-    <?php  endforeach;?>
-  </ul>
+  <div class="col-md-3">
+    <ul class="nav nav-stacked">
+      <?php foreach ($catPost as $post) : setup_postdata($post); ?>
+        <li class="cat-tab-nav"><a href="<?php echo '#' . $post->post_name ?>"><?php the_title(); ?></a></li>
+        <?php $i++ ?>
+      <?php  endforeach;?>
+    </ul>
+  </div>
   <div class="tab-content col-md-9">
+    
     <?php $j = 1; ?>
     <?php foreach ($catPost as $post) : setup_postdata($post); ?>
       <div class="single-cat-post" id="<?php echo $post->post_name ?>">
-        <h2><?php the_title(); ?></h2>
+        <h1><?php the_title(); ?></h1>
+        <a href="#top">Back to Top</a>
         <?php the_content(); ?>
         <?php if ( count($catPost) != $j ) echo '<hr>'; ?>
       </div>
